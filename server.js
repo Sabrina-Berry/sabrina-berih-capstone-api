84b138f1517fd20c0452c6b2ef541b94f7abde0e
Sabrina B <sabrinaberih@gmail.com>
2024-10-24 22:18:01 -0400

    Complete back-end for about page

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-
+import fs from "fs";
+''
 const { PORT, BACKEND_URL } = process.env;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static("public"));
+
+// Read data from about.json
+  function readAbout() {
+    const aboutData = fs.readFileSync("./data/about.json");
+    const parsedData = JSON.parse(aboutData);
+    return parsedData;
+  }
+
+app.get('/', (req, res) => {
+    res.send("Welcome to my website 👋");
+});
+
+// Route for about page
+app.get("/about", (req, res) => {
+  const about = readAbout();
+  // send about page data as a json with res
+  res.json(about);
+});
 
 app.listen(PORT, () => {
     console.log(`Listening at ${BACKEND_URL}:${PORT}`);
